d8f4a2a45bc02d0eb226e2343e10e98682c36404
Ozenrol <ozenrol2@gmail.com>
2022-06-04 15:03:21 +0200

    Add listeners for missing permissions

--- src/events/commands/clientMissingPermissions.ts
+++ src/events/commands/clientMissingPermissions.ts
@@ -4,14 +4,14 @@ import type { CommandInteraction } from "discord.js";
 
 export class MissingPermissions extends Event {
   constructor(client: ShewenyClient) {
-    super(client, "userMissingPermissions", {
-      description: "User is missing permissions",
+    super(client, "clientMissingPermissions", {
+      description: "Client is missing permissions",
       once: false,
       emitter: client.managers.commands
     });
   }
 
   async execute(interaction: CommandInteraction, missingPermissions: string[]) {
-    return await interaction.reply(`Il te manque l${missingPermissions.length > 1 ? 'es' : 'a'} permission${missingPermissions.length > 1 ? 's' : ''} \`${missingPermissions}\` pour la commande **${interaction.commandName}**`);
+    return await interaction.reply(`Il me manque l${missingPermissions.length > 1 ? 'es' : 'a'} permission${missingPermissions.length > 1 ? 's' : ''} \`${missingPermissions}\` pour la commande **${interaction.commandName}**`);
   }
 }
